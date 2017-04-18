35f06b6dd2f0bf3147f87b31f9bd6a6cc32fa1cb
Flor Tello <flor.26071994@gmail.com>
2017-04-18 11:50:08 -0500

    Agregando estilo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
-var arrayImgs = [{url:"assets/img/panda1.jpg", alt:'panda' },
-                 {url:"assets/img/panda2.jpg", alt:'panda' },
-                 {url:"assets/img/panda3.jpg", alt:'panda' },
-                 {url:"assets/img/panda4.jpg", alt:'panda' }
+var arrayImgs = [{url:"assets/img/panda1.jpg", alt:'panda' },{url:"assets/img/panda2.jpg", alt:'panda' },
+                 {url:"assets/img/panda3.jpg", alt:'panda' },{url:"assets/img/panda4.jpg", alt:'panda' }
                ];
 var dfm = document.createDocumentFragment();
 window.addEventListener('load',function(){
@@ -34,7 +32,8 @@ function crearImgPanda(elemento){
   img.setAttribute('src',elemento.url);
   img.setAttribute('alt',elemento.alt);
   span.addEventListener('click',function(e){
-    e.target.parentNode.classList.toggle('displayNone');
+    this.parentNode.classList.toggle('displayNone');
+    // e.target.parentNode.classList.toggle('displayNone');
     // e.target.parentNode.parentNode.removeChild(e.target.parentNode);
   });
   figure.appendChild(img);
